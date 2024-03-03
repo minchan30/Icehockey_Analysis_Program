fb87f57a59ebb5721a871d9adf5b93cf5d9e5457
minchan30 <goalie0901@gmail.com>
2024-03-03 12:33:45 +0900

    :tada:successd

--- Project/Icehockey_Analysis_Program.py
+++ Project/Icehockey_Analysis_Program.py
@@ -13,59 +13,82 @@ class MyApp(QWidget):
     def initUI(self):
         self.setWindowTitle('IceHockey')
         self.setWindowIcon(QIcon('hockey_icon.png'))
+
+        ice_rink = QLabel(self)
+        ice_rink_pixmap = QPixmap('ice_rink.png')
+        ice_rink_pixmap = ice_rink_pixmap.scaled(1400, 700)
+        ice_rink.setPixmap(ice_rink_pixmap)
         
         black_box = QLabel(self)
         black_box_pixmap = QPixmap('black.png')
         black_box_pixmap = black_box_pixmap.scaled(300, 370)
         black_box.setPixmap(black_box_pixmap)
-        
-        btn_start = QPushButton('시작 \n (space)', self)
-        btn_start.setFixedSize(230, 70)
-        btn_start.setToolTip('This is a start widget')
-        
-        btn_change_time = QPushButton('시간변경', self)
-        btn_change_time.setToolTip('This is a change time widget')
-        
-        inner_vbox = QVBoxLayout()
-        inner_vbox.addWidget(btn_start)
-        inner_vbox.addWidget(btn_change_time)
 
         black_box2 = QLabel(self)
         black_box_pixmap2 = QPixmap('black.png')
         black_box_pixmap2 = black_box_pixmap2.scaled(300, 320)
         black_box2.setPixmap(black_box_pixmap2)
+        
+        btn_start = QPushButton('시작 \n (space)', self)
+        btn_start.setFixedSize(290, 75)
+        btn_start.setToolTip('This is a start widget')
+        
+        btn_change_time = QPushButton('시간변경', self)
+        btn_change_time.setFixedSize(290, 35)
+        btn_change_time.setToolTip('This is a change time widget')
+
+        btn_home_plus = QPushButton('홈 스코어 증가 \n +', self)
+        btn_home_plus.setFixedSize(290, 75)
+        btn_home_plus.setToolTip('This is a home score plus widget')
+
+        btn_home_minus = QPushButton('홈 스코어 감소 \n -', self)
+        btn_home_minus.setFixedSize(290, 75)
+        btn_home_minus.setToolTip('This is a home score minus. widget')
+
+        btn_away_plus = QPushButton('어웨이 스코어 증가 \n +', self)
+        btn_away_plus.setFixedSize(290, 75)
+        btn_away_plus.setToolTip('This is a away score plus widget')
+
+        btn_away_minus = QPushButton('어웨이 스코어 감소 \n -', self)
+        btn_away_minus.setFixedSize(290, 75)
+        btn_away_minus.setToolTip('This is a away score minus widget')
+#__________________________________________________________________________________________________________
+        # 1-2번째
+        inner_vbox = QVBoxLayout()
+        inner_vbox.addWidget(btn_start)
+        inner_vbox.addWidget(btn_change_time)
 
         inner_vbox2 = QVBoxLayout()
         inner_vbox2.addStretch(1)
         inner_vbox2.addWidget(black_box2)
 
+        inner_vbox3 = QVBoxLayout()
+        inner_vbox3.addWidget(btn_home_plus)
+        inner_vbox3.addWidget(btn_home_minus)
+
+        inner_vbox4 = QVBoxLayout()
+        inner_vbox4.addWidget(btn_away_plus)
+        inner_vbox4.addWidget(btn_away_minus)
+
         hbox = QHBoxLayout()
-        hbox.addStretch(1)
+        hbox.addStretch(30)
         hbox.addWidget(black_box)
+        hbox.addStretch(30)
         hbox.addLayout(inner_vbox)
+        hbox.addStretch(30)
         hbox.addLayout(inner_vbox2)
-        hbox.addStretch(100)
+        hbox.addStretch(30)
+        hbox.addLayout(inner_vbox3)
+        hbox.addStretch(30)
+        hbox.addLayout(inner_vbox4)
+        hbox.addStretch(1000)
+
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
+        hbox.addStretch(30)
+        vbox.addWidget(ice_rink)
         vbox.addStretch(1)
-
-        ice_rink = QLabel(self)
-        ice_rink = QPixmap('ice_rink.png')
-        ice_rink_pixmap = ice_rink_pixmap.scaled(300, 370)
-        ice_rink.setPixmap(ice_rink)
-
-        inner_vbox3 = QVBoxLayout()
-        inner_vbox3.addStretch(1)
-        inner_vbox.addWidget(ice_rink)
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(black_box)
-        hbox.addLayout(inner_vbox)
-        hbox.addLayout(inner_vbox2)
-        hbox.addStretch(100)
-
 
 
         self.setLayout(vbox)
@@ -76,3 +99,22 @@ if __name__ == '__main__':
     myApp = MyApp()
     myApp.show()
     sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+
+
+
+     # vbox = QVBoxLayout()
+        # vbox.addLayout(hbox)
+        # vbox.addStretch(1)
+        # vbox.addWidget(ice_rink)
+
+        # # 두 번째 행 위젯 추가
+        # widget2 = QLabel("첫 번째 행")
+        # whole_vbox.addWidget(widget2)
